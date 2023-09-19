9c102ca6dd0ad9d841648db2b905b74da979ec78
dumaphilile <duma.phile@gmail.com>
2023-09-19 14:31:15 +0200

    Added new route, update GettingStarted2

--- controllers/core_controller.py
+++ controllers/core_controller.py
@@ -5,6 +5,9 @@ from flask import render_template
 def studentAttendance():
     return render_template("layouts/student/StudentAttendance_layout.html")
 
+def gettingstarted2():
+    return render_template("layouts/student/GettingStarted2_layout.html", dropdown_items=[{'id':"chk-PRJB301", "name":"Project B 301","level":"3","code":"PRJB301"}], tags=[], limit=3)
+
 def index():
     dropdown_items = [{'id':'chk-0', 'name' : 'BACHELOR OF INF & COM TECHNOLOGY', 'level' : '3', 'code' : 'BINCT'},{'id':'chk-1','name':'DIPLOMA IN ICT APPLICATIONS DEVELOPMENT','level': '2', 'code' : 'DIIAD1'}];
     limit = 2
--- routes/core_route.py
+++ routes/core_route.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from controllers.mutliselect_controller import handleMultiSelect, handleSearchField, getAllItems # Multiselect Control - by m-mngadi
-from controllers.core_controller import index, studentAttendance
+from controllers.core_controller import index, studentAttendance, gettingstarted2
 core_route = Blueprint('core_routes',__name__)
 
 core_route.get("/") (index)
 core_route.get('/student_attendance')(studentAttendance)
+core_route.get("/getting_started2")(gettingstarted2)
 # Multiselect Control - by m-mngadi
 core_route.get("/dropdown-items")(getAllItems)
 core_route.post("/dropdown-config") (handleMultiSelect)
