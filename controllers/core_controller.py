da0e1e108d6abb1a413b81074c319fbd7d2be090
Karabo Ntholeng <karabelontholeng04@gmail.com>
2023-09-20 18:07:13 +0200

    Added sigin route, assigend sign in endpoint

--- controllers/core_controller.py
+++ controllers/core_controller.py
@@ -6,3 +6,6 @@ def index():
     dropdown_items = [{'id':'chk-0', 'name' : 'BACHELOR OF INF & COM TECHNOLOGY', 'level' : '3', 'code' : 'BINCT'},{'id':'chk-1','name':'DIPLOMA IN ICT APPLICATIONS DEVELOPMENT','level': '2', 'code' : 'DIIAD1'}];
     limit = 2
     return render_template('layouts/LandingPage_layout.html', dropdown_items=dropdown_items,tags=[],limit=limit)
+
+def signIn ():
+    return render_template('layouts/SignIn_layout.html')
--- routes/core_route.py
+++ routes/core_route.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from controllers.mutliselect_controller import handleMultiSelect, handleSearchField, getAllItems # Multiselect Control - by m-mngadi
-from controllers.core_controller import index
+from controllers.core_controller import index, signIn
 core_route = Blueprint('core_routes',__name__)
 
 core_route.get("/") (index)
+core_route.get('/sign_in')(signIn)
 
 
 # Multiselect Control end.
