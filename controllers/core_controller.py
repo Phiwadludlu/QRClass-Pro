c73b3565de0fa37f94fe3cfaea478ff6855e07b2
Mziwenhlanhla Mngadi <mngadimziwenhlanhla@gmail.com>
2023-09-27 01:10:09 +0200

    Remove "datafrom" from core_controller, not used

--- controllers/core_controller.py
+++ controllers/core_controller.py
@@ -9,9 +9,9 @@ def index():
 def gettingStarted1():
     qualificationData = getAllQualifications()
     limit = 1
-    return render_template('layouts/student/GetttingStarted1_layout.html', dropdown_items=qualificationData, limit=limit, tags=[], datafrom="qualifications")
+    return render_template('layouts/student/GetttingStarted1_layout.html', dropdown_items=qualificationData, limit=limit, tags=[])
 
 def gettingStarted2():
     moduleData = getAllModules()
     limit = 5
-    return render_template('layouts/student/GettingStarted2_layout.html', dropdown_items=moduleData, limit=limit, tags=[], datafrom="modules")
+    return render_template('layouts/student/GettingStarted2_layout.html', dropdown_items=moduleData, limit=limit, tags=[])
