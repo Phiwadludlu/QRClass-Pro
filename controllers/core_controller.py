8b251d49f60fbd94784d699ed73505ae70c1e8a6
Mziwenhlanhla Mngadi <mngadimziwenhlanhla@gmail.com>
2023-09-27 14:12:59 +0200

    Reordered core routes

--- controllers/core_controller.py
+++ controllers/core_controller.py
@@ -3,12 +3,6 @@
 from flask import render_template
 from controllers.mutliselect_controller import getAllQualifications, getAllModules
 
-def studentAttendance():
-    return render_template("layouts/student/StudentAttendance_layout.html")
-
-def gettingstarted2():
-    return render_template("layouts/student/GettingStarted2_layout.html", dropdown_items=[{'id':"chk-PRJB301", "name":"Project B 301","level":"3","code":"PRJB301"}], tags=[], limit=3)
-
 def index():
     return render_template('layouts/LandingPage_layout.html')
 
@@ -21,3 +15,6 @@ def gettingStarted2():
     moduleData = getAllModules()
     limit = 5
     return render_template('layouts/student/GettingStarted2_layout.html', dropdown_items=moduleData, limit=limit, tags=[])
+
+def studentAttendance():
+    return render_template("layouts/student/StudentAttendance_layout.html")
