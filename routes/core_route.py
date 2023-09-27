da685524e99f7e2f357149f1368f804353133bf8
Phiwa Dludlu <phiwadludlu79@gmail.com>
2023-09-27 14:28:13 +0200

    fixed bugs from moving student logic to controller

--- routes/core_route.py
+++ routes/core_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from controllers.core_controller import index, studentAttendance, gettingStarted1, gettingStarted2
+from controllers import core_controller as core_controller
+
 core_route = Blueprint('core_routes',__name__)
 
-core_route.get("/") (index)
+core_route.get("/") (core_controller.index)
