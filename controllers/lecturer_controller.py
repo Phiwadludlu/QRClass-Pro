33203985272ed9666209a3c2b451d15850eff0ee
Mziwenhlanhla Mngadi <mngadimziwenhlanhla@gmail.com>
2023-09-29 20:30:15 +0200

    Added table macro and made it responsive

--- controllers/lecturer_controller.py
+++ controllers/lecturer_controller.py
@@ -2,3 +2,16 @@ from flask import render_template
 
 def viewAttendance():
     return render_template('layouts/lecturer/LecturerAttendance.html')
+
+def activeQR():
+    qr_data = [{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Software Engineering III","session":"30/09/2023 09:30 AM","expiration":"30/09/2023 12:00 PM","expired":0,"qr_url":"path/to/blob/storage"},
+{"module":"Project B III","session":"28/09/2023 14:00 AM","expiration":"28/09/2023 12:00 PM","expired":1,"qr_url":"path/to/blob/storage"},
+{"module":"Project B III","session":"28/09/2023 14:00 AM","expiration":"28/09/2023 12:00 PM","expired":1,"qr_url":"path/to/blob/storage"},
+{"module":"Project B III","session":"28/09/2023 14:00 AM","expiration":"28/09/2023 12:00 PM","expired":1,"qr_url":"path/to/blob/storage"}]
+    return render_template('layouts/lecturer/ActiveQR_layout.html', qr_data=qr_data)
--- routes/lecturer_route.py
+++ routes/lecturer_route.py
@@ -4,3 +4,4 @@ from controllers import lecturer_controller as le_controller
 lecturer_route = Blueprint("lecturers", __name__)
 
 lecturer_route.get('/attendance') (le_controller.viewAttendance)
+lecturer_route.get('/qr/active') (le_controller.activeQR)
