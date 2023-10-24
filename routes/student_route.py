da9a75c48694e46aa636219b55a2d6cb8ca4eadd
Mziwenhlanhla Mngadi <mngadimziwenhlanhla@gmail.com>
2023-10-24 10:07:05 +0200

    Final

--- routes/student_route.py
+++ routes/student_route.py
@@ -5,7 +5,7 @@ student_route = Blueprint("students", __name__)
 
 student_route.get("/")(sc.StudentMain)
 student_route.get('/attendance')(sc.studentAttendance)
-student_route.get('/getting-started')(sc.gettingStarted)
+#student_route.get('/getting-started')(sc.gettingStarted)
 student_route.get('/qr/scan')(sc.scanQR)
 
 student_route.get('/manage') (sc.manage)
